47f76d6936fb5ab1f459d08ec538d6fa5a7519e0
ParkYunHo <qkrdbsgh0921@gmail.com>
2023-03-29 21:48:10 +0900

    :sparkles:feature : AuthenticationEntryPoint 구현

--- src/main/kotlin/com/yoonho/oauth/config/SecurityConfig.kt
+++ src/main/kotlin/com/yoonho/oauth/config/SecurityConfig.kt
@@ -16,32 +16,47 @@ import org.springframework.security.web.SecurityFilterChain
 class SecurityConfig {
     private val log = LoggerFactory.getLogger(this::class.java)
 
-    @Bean
-    fun formLoginConfigure(http: HttpSecurity): SecurityFilterChain {
+//    /**
+//     * 다중 SecurityFilterChain 구현
+//     * <p>
+//     *     - formLoginConfigure: FormLogin 설정
+//     *     - httpBasicConfigure: HttpBasic 설정
+//     *
+//     * @author yoonho
+//     * @since 2023.03.28
+//     */
+//    @Bean
+//    fun formLoginConfigure(http: HttpSecurity): SecurityFilterChain {
+//
+//        http
+//            .formLogin()
+//
+//        http
+//            .authorizeHttpRequests()
+//            .anyRequest().authenticated()
+//
+//        return http.build()
+//    }
+//
+//    @Bean
+//    fun httpBasicConfigure(http: HttpSecurity): SecurityFilterChain {
+//
+//        http
+//            .httpBasic()
+//
+//        http
+//            .authorizeHttpRequests()
+//            .anyRequest().authenticated()
+//
+//        return http.build()
+//    }
 
-        http
-            .formLogin()
-
-        http
-            .authorizeHttpRequests()
-            .anyRequest().authenticated()
-
-        return http.build()
-    }
-
-    @Bean
-    fun httpBasicConfigure(http: HttpSecurity): SecurityFilterChain {
-
-        http
-            .httpBasic()
-
-        http
-            .authorizeHttpRequests()
-            .anyRequest().authenticated()
-
-        return http.build()
-    }
-
+//    /**
+//     * CustomSecurityConfigurer 구현
+//     *
+//     * @author yoonho
+//     * @since 2023.03.28
+//     */
 //    @Bean
 //    fun configure(http: HttpSecurity): SecurityFilterChain {
 //
@@ -57,4 +72,35 @@ class SecurityConfig {
 //
 //        return http.build()
 //    }
+
+    /**
+     * AuthenticationEntryPoint 설정
+     * <p>
+     *     - FormLogin, HttpBasic 둘다 설정하지 않은 경우, AuthenticationEntryPoint 호출됨
+     *     - FormLogin, HttpBasic 둘다 설정한 경우, Spring Security가 내부적으로 체크하여 FormLogin()을 호출함
+     *
+     * @author yoonho
+     * @since 2023.03.28
+     */
+    @Bean
+    fun configure(http: HttpSecurity): SecurityFilterChain {
+
+        http
+            .formLogin()
+        http
+            .httpBasic()
+
+        http
+            .exceptionHandling()
+            .authenticationEntryPoint { request, response, authException ->
+                // commence() 메서드 override
+                log.info(" >>> [commence] Custom EntryPoint!!")
+            }
+
+        http
+            .authorizeHttpRequests()
+            .anyRequest().authenticated()
+
+        return http.build()
+    }
 }
