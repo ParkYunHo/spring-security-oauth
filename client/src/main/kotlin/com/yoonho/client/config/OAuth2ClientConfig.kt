400d9cec6984f7423c5b6e4c0a51865ab855ee3a
ParkYunHo <qkrdbsgh0921@gmail.com>
2023-04-11 22:52:21 +0900

    :sparkles:feature : OAuth2Client 자동설정 구현

--- client/src/main/kotlin/com/yoonho/client/config/OAuth2ClientConfig.kt
+++ client/src/main/kotlin/com/yoonho/client/config/OAuth2ClientConfig.kt
@@ -2,22 +2,26 @@ package com.yoonho.client.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.oauth2.client.registration.ClientRegistration
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.registration.ClientRegistrations
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
+import org.springframework.security.web.SecurityFilterChain
 
 /**
  * @author yoonho
  * @since 2023.04.10
  */
+@EnableWebSecurity
 @Configuration
 class OAuth2ClientConfig {
 
     /**
      * ClientRegistrationRepository Bean 설정
      * <p>
-     *     - default repository구현체인 InMemoryClientRegistrationRepository 방식 사용
+     *     - default repository 구현체인 InMemoryClientRegistrationRepository 방식 사용
      *
      * @author yoonho
      * @since 2023.04.10
@@ -41,4 +45,16 @@ class OAuth2ClientConfig {
             .clientSecret("oTzOtDvPQr7R2yxtJKOHN4gB3FNTKpfw")
             .redirectUri("http://localhost:8080/login/oauth2/code/keycloak")
             .build()
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests()
+            .anyRequest().authenticated()
+
+        http
+            .oauth2Client()
+
+        return http.build()
+    }
 }
